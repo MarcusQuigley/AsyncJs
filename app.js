4948f0a83976f8dca799003f5f0e6423d2a43806
marcus quigley <marcusquigley@gmail.com>
2019-11-09 20:18:52 -0500

    adding XHR object functionality

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-document.getElementById('button').addEventListener('click', loadData);
+document.getElementById('xhrButton').addEventListener('click', loadXhrData);
 
-function loadData(e) {
-	console.log('bang');
-	e.preventDefault();
+function loadXhrData(e) {
+	const xhr = new XMLHttpRequest();
+
+	xhr.open("GET", 'data.txt', true);
+
+	xhr.onload = function () {
+		if (this.status === 200) {
+			document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`
+			console.log(this.responseText)
+		}
+		else {
+			console.log(`Error occured: ${this.status}`)
+		}
+	}
+
+	xhr.send();
 }
